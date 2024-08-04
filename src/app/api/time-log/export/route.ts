e780c0a8ed28c4fcfd8aeeba401f0d16fa7098b4
Anderson-Portes <portsanderson@gmail.com>
2024-08-04 10:30:41 -0300

    changing time-log export sql to work at a postgresql database

--- src/app/api/time-log/export/route.ts
+++ src/app/api/time-log/export/route.ts
@@ -7,7 +7,7 @@ export const GET = async (req: NextRequest) => {
   const initialDate = req.nextUrl.searchParams.get("initialDate");
   const finalDate = req.nextUrl.searchParams.get("finalDate");
   const hoursUsed = await prisma.$queryRaw`
-    SELECT description,sum((finishedAt - createdAt) / 3600000.0)  AS hours_used FROM TimeLog 
-    WHERE userId = ${token?.sub} and DATE(createdAt / 1000,'unixepoch') between ${initialDate} and ${finalDate}`;
+    SELECT description, sum(finishedAt - createdAt) AS hours_used FROM TimeLog 
+    WHERE userId = ${token?.sub} createdAt between ${initialDate} and ${finalDate}`;
   return NextResponse.json(hoursUsed);
 };
