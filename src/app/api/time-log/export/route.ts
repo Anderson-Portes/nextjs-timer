192cb5fe545c9d07edeb91882ebe2ea49942b44f
Anderson Portes do Nascimento <96482967+Anderson-Portes@users.noreply.github.com>
2024-08-07 09:28:28 -0300

    chaging data type of dates in sql query

--- src/app/api/time-log/export/route.ts
+++ src/app/api/time-log/export/route.ts
@@ -4,13 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const token = await getToken({ req });
-  const initialDateParam = req.nextUrl.searchParams.get("initialDate");
-  const finalDateParam = req.nextUrl.searchParams.get("finalDate");
-  const initialDate = initialDateParam ? new Date(initialDateParam) : new Date();
-  const finalDate = finalDateParam ? new Date(finalDateParam) : new Date();
+  const initialDate = req.nextUrl.searchParams.get("initialDate");
+  const finalDate = req.nextUrl.searchParams.get("finalDate");
   const hoursUsed = await prisma.$queryRaw`
     SELECT description, sum("finishedAt" - "createdAt") AS hours_used FROM "TimeLog" 
-    WHERE "userId" = ${token?.sub} and "createdAt"::date between ${initialDate} and ${finalDate} group by description
+    WHERE "userId" = ${token?.sub} and "createdAt"::date between ${initialDate}::date and ${finalDate}::date group by description
   `;
   return NextResponse.json(hoursUsed);
 };
