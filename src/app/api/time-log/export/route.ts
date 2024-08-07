635f195f1af51217036d7f5b0054ef8bd73d38ba
Anderson Portes do Nascimento <96482967+Anderson-Portes@users.noreply.github.com>
2024-08-07 09:25:00 -0300

    verifying date params

--- src/app/api/time-log/export/route.ts
+++ src/app/api/time-log/export/route.ts
@@ -4,8 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const token = await getToken({ req });
-  const initialDate = new Date(req.nextUrl.searchParams.get("initialDate"));
-  const finalDate = new Date(req.nextUrl.searchParams.get("finalDate"));
+  const initialDateParam = req.nextUrl.searchParams.get("initialDate");
+  const finalDateParam = req.nextUrl.searchParams.get("finalDate");
+  const initialDate = initialDateParam ? new Date(initialDateParam) : new Date();
+  const finalDate = finalDateParam ? new Date(finalDateParam) : new Date();
   const hoursUsed = await prisma.$queryRaw`
     SELECT description, sum("finishedAt" - "createdAt") AS hours_used FROM "TimeLog" 
     WHERE "userId" = ${token?.sub} and "createdAt"::date between ${initialDate} and ${finalDate} group by description
