d15913c61af1708f7e287aa48837a53c90ec1c99
Anderson Portes do Nascimento <96482967+Anderson-Portes@users.noreply.github.com>
2024-08-07 09:21:41 -0300

    changing data data type of initialDate and finalDate variables

--- src/app/api/time-log/export/route.ts
+++ src/app/api/time-log/export/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const token = await getToken({ req });
-  const initialDate = req.nextUrl.searchParams.get("initialDate");
-  const finalDate = req.nextUrl.searchParams.get("finalDate");
+  const initialDate = new Date(req.nextUrl.searchParams.get("initialDate"));
+  const finalDate = new Date(req.nextUrl.searchParams.get("finalDate"));
   const hoursUsed = await prisma.$queryRaw`
     SELECT description, sum("finishedAt" - "createdAt") AS hours_used FROM "TimeLog" 
     WHERE "userId" = ${token?.sub} and "createdAt"::date between ${initialDate} and ${finalDate} group by description
